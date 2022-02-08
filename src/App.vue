38e7893589247c119acf64595a76085245d6e82f
Hyperbola <me@hyperbola.me>
2022-02-08 22:45:02 +0800

    update homepage

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <header class="p-1 mb-4">
+    <toxicpie-row :row="1"></toxicpie-row>
+  </header>
+  <main class="grow">
+    <palette class="h-full"></palette>
+  </main>
+  <footer class="p-1 mt-4">
+    <toxicpie-row :row="1"></toxicpie-row>
+  </footer>
 </template>
+
+<script setup lang="ts">
+import Palette from '@/components/Palette.vue'
+import ToxicpieRow from '@/components/ToxicpieRow.vue'
+</script>
 
 <style lang="scss">
 @import "@/assets/scss/main";
 
+html,
+body {
+  @apply h-full bg-zinc-100;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  @apply flex flex-col;
+
+  // https://medium.com/@littleDog/mobile-view-height%E7%9A%84%E9%80%9A%E9%80%8F%E4%B8%96%E7%95%8C-b896ac234ba9
+  height: 100%;
+  height: -moz-available;
+  height: -webkit-fill-available;
+  height: stretch;
 }
 </style>
